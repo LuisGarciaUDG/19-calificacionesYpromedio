c964a1eff2cc3ac72a443792d1e33c9606c2110e
Luis Alberto García Rodríguez <luis.gcdz@gmail.com>
2018-04-22 19:37:30 -0500

    Ciclos: while

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/* programa_19 Imprimir los numeros entre el 0 y el 20
+estructura: ciclica
+autor: Luis Alberto Garcia Rodriguez
+numero de lista 11
+fecha 24 de febrero del 2015
+*/
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    float prom, s, a;
+    s=0;
+    x=0;
+    cout<<"\n Claificacion Promedio\n\n";
+    while (x<=4)
+    {
+    cout<<"Escribe la calificacion: ";
+    cin>>a;
+    cout<<"\n";
+    s=s+a;
+    x=x+1;
+    }
+    prom=s/5;
+    cout<<"\t\t\t\t El promedio es: " <<prom<<"\n\n\n\t\t\t\t\t";
+    system ("pause");
+
+    return 0;
+}
